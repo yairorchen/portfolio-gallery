bc47b739ff27fb2536b1eb3759b0e9eba2a0963f
Yair Orchen <yairorchen1@gmail.com>
2022-10-13 01:00:20 +0300

    changed something

--- js/gallery.service.js
+++ js/gallery.service.js
@@ -4,14 +4,14 @@ console.log('gallery service')
 
 var gProjects
 
-function addProjects() {
+function createProjects() {
   const projects = [
     {
       id: 'mine-sweeper',
       name: 'mine sweeper',
       title: 'save yourself from the mine!',
       desc: makeLorem(15),
-      url: 'projs/mine-sweeper',
+      url: 'https://yairorchen.github.io/mine-sweeper-casep22-step1/',
       publishedAt: 'september 2022',
       labels: ['matrixes', 'keyboard event'],
     },
@@ -20,7 +20,7 @@ function addProjects() {
       name: 'touch numbers',
       title: 'touch numbers by place!',
       desc: makeLorem(15),
-      url: 'projs/touch-nums',
+      url: 'https://yairorchen.github.io/touch-num/',
       publishedAt: 'september 2022',
       labels: ['matrixes', 'keyboard event'],
     },
@@ -29,7 +29,7 @@ function addProjects() {
       name: 'book shop',
       title: 'manage your book shop!',
       desc: makeLorem(15),
-      url: 'projsq/book-shop',
+      url: 'https://yairorchen.github.io/book-shop/',
       publishedAt: 'september 2022',
       labels: ['matrixes', 'keyboard event'],
     },
@@ -39,9 +39,24 @@ function addProjects() {
 }
 
 function getPortfolio() {
-  addProjects()
-  console.log(gProjects)
+  createProjects()
   return gProjects
 }
 
-getPortfolio()
+function getArticleById(articleId) {
+  var article = gProjects.find((article) => articleId === article.id)
+  return article
+}
+
+function sendEmail(adress, subject, message) {
+  var strEMAIL =
+    'https://mail.google.com/mail/?view=cm&fs=1&to=' +
+    adress +
+    '&su=' +
+    subject +
+    '&body=' +
+    message +
+    ''
+  console.log(JSON.stringify(strEMAIL))
+  console.log(strEMAIL)
+}
--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 'use strict'
 
 console.log('controller')
-console.log(document.querySelector('.portfolio-articles'))
 
-renderPortfolio()
+function onInit() {
+  renderPortfolio()
+}
+
 function renderPortfolio() {
   const portfolio = getPortfolio()
-  console.log(portfolio)
   var strHtmls = portfolio.map(
-    (article) => `
-    <div class="row">
+    (article) =>
+      `
+    
         <div class="col-md-4 col-sm-6 portfolio-item">
-          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${article.id}">
+          <a onclick="updateModal('${article.id}')" class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
                 <i class="fa fa-plus fa-3x"></i>
               </div>
             </div>
-            <img class="img-fluid" src="img/portfolio/${article.id}.png" alt="">
+            <img class="img-fluid" src="img/portfolio/${article.id}.jpeg" alt="">
           </a>
           <div class="portfolio-caption">
             <h4>${article.name}</h4>
@@ -29,64 +31,55 @@ function renderPortfolio() {
 
   document.querySelector('.portfolio-articles').innerHTML = strHtmls.join('')
 }
-renderModals()
-function renderModals() {
-  const portfolio = getPortfolio()
-  console.log(portfolio)
-  var strHtmls = portfolio.map(
-    (article) => `
-      <div class="portfolio-modal modal fade" id="portfolioModal${article.id}" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="close-modal" data-dismiss="modal">
-          <div class="lr">
-            <div class="rl"></div>
-          </div>
-        </div>
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 mx-auto">
-              <div class="modal-body">
-                <!-- Project Details Go Here -->
-                <h2>${article.name}</h2>
-                <p class="item-intro text-muted">${article.title}</p>
-                <img class="img-fluid d-block mx-auto" src="img/portfolio/${article.id}.png" alt="">
-                <p>${article.desc}</p>
-                <ul class="list-inline">
-                  <li class="article-date" >Date:${article.publishedAt}</li>
-                  <li>Client: Threads</li>
-                  <li class="article-category">Category:${article.labels}</li>
-                </ul>
-                <button class="btn btn-primary" data-dismiss="modal" type="button">
-                  <i class="fa fa-times"></i>
-                  Close Project</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-            `
-  )
+// function renderPortfolio() {
+//   const portfolio = getPortfolio()
+//   var strHtmls = portfolio.map(
+//     (article) =>
+//       `
+//     <div class="row">
+//         <div class="col-md-4 col-sm-6 portfolio-item">
+//           <a onclick="updateModal('${article.id}')" class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+//             <div class="portfolio-hover">
+//               <div class="portfolio-hover-content">
+//                 <i class="fa fa-plus fa-3x"></i>
+//               </div>
+//             </div>
+//             <img class="img-fluid" src="img/portfolio/${article.id}.jpeg" alt="">
+//           </a>
+//           <div class="portfolio-caption">
+//             <h4>${article.name}</h4>
+//             <p class="text-muted">${article.title}</p>
+//           </div>
+//         </div>
+//             `
+//   )
 
-  document.querySelector('.modals-container').innerHTML = strHtmls.join('')
+//   document.querySelector('.portfolio-articles').innerHTML = strHtmls.join('')
+// }
+
+function updateModal(projId) {
+  const currProject = getArticleById(projId)
+  console.log(currProject)
+  document.querySelector('.modal-body h2').innerHTML = currProject.name
+  document.querySelector('.modal-body .item-intro').innerHTML =
+    currProject.title
+  document.querySelector(
+    '.modal-body .article-img'
+  ).innerHTML = `<img class="img-fluid d-block mx-auto" src="img/portfolio/${currProject.id}.jpeg" alt="">`
+  document.querySelector('.modal-body p').innerHTML = currProject.desc
+  document.querySelector('.modal-body .article-date').innerHTML =
+    'Date :  ' + currProject.publishedAt
+  document.querySelector('.modal-body .article-category').innerHTML =
+    'Category : ' + currProject.labels
+
+  document.querySelector(
+    '.modal-body .git-link'
+  ).innerHTML = `<a href="${currProject.url}" target="_blank"> go play</a>`
 }
 
-// document.querySelector('.modal-body h2').innerHTML=article.name
-// //                 <h2>${article.name}</h2>
-
-// document.querySelector('.modal-body p .item-intro').innerHTML=article.title
-// //                 <p class="item-intro text-muted">${article.title}</p>
-
-// document.querySelector('.modal-body p .article-img').innerHTML =
-//   //                 <img class="img-fluid d-block mx-auto" src="img/portfolio/${article.id}.png" alt="">
-
-//   document.querySelector('.modal-body p').innerHTML = article.desc
-// //                 <p>${article.desc}</p>
-
-// document.querySelector('.modal-body li .article-date').innerHTML = article.publishedAt
-// //                   <li>Date:${article.publishedAt}</li>
-
-// document.querySelector('.modal-body li .article-category').innerHTML =article.labels
-// //                   <li>Category:${article.labels}</li>
+function onSendEmail() {
+  const ADRESS = document.querySelector('.message-email').value
+  const SUBJECT = document.querySelector('.message-subject').value
+  const MESSAGE = document.querySelector('.message-body').value
+  sendEmail(ADRESS, SUBJECT, MESSAGE)
+}
